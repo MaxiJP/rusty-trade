3735715b62e20f37a639e2fc8b84f608660c3f43
maxijp <max.r.prime@gmail.com>
2025-06-01 09:49:22 +0100

    Removing logging for pings, its annoying as shit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use tungstenite::{connect, Message};
-use serde_json::{Result, Value};
+use serde_json::Value;
 use colored::Colorize;
 use chrono::Local;
 
 
 fn main() {
 
-    let (mut socket, response) = connect("ws://ws.rugplay.com/api/").expect("Can't connect");
+    let (mut socket, _response) = connect("ws://ws.rugplay.com/api/").expect("Can't connect");
 
     socket.send(Message::Text("{\"type\":\"subscribe\",\"channel\":\"trades:all\"}".into())).unwrap();
     socket.send(Message::Text("{\"type\":\"set_coin\",\"coinSymbol\":\"@global\"}".into())).unwrap();
@@ -35,20 +35,21 @@ fn main() {
             single_coin_price = v["data"]["price"].clone().as_f64();
             trade_type = v["type"].clone();
 
-            let mut trade_type_print = trade_type.as_str().as_slice()[0];
-
-            if trade_type_print == "ping" {
-                println!("[ping] Ping!");
-                break;
-            }
+            let trade_type_print = trade_type.as_str().as_slice()[0];
             let date = Local::now();
             let date_print = date.format("%H:%M:%S");
+
+            if trade_type_print == "ping" {
+                break;
+            }
+            
             let amount_print = amount.as_slice()[0];
             let coin_print = coin.as_str().as_slice()[0];
             let total_buy_print = total_buy.as_slice()[0];
             let single_coin_price_print = single_coin_price.as_slice()[0];
             let username_print = username.as_str().as_slice()[0];
             
+            if trade_type_print == "live-trade" {}
 
             if sell == true {
                 println!("{} [{}] {} {} {:.2} {} (${:.2}) @ ${:.8}", date_print, trade_type_print, "SELL".red(), username_print, amount_print, coin_print, total_buy_print, single_coin_price_print);
